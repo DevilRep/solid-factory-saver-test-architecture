b53044b9cfb263db834455fca800a1ea51062eab
Yevhen Tkachuk <flaringrep@gmail.com>
2026-09-22 22:02:50 +0200

    feat(e2): update the implementation for the repository

--- src/repositories/user.memory.repository.ts
+++ src/repositories/user.memory.repository.ts
@@ -1,20 +1,35 @@
-import { IUserRepository, IUserRepositoryData } from "@domain/user";
+import { IUserRepository, IUserRepositoryData, IUserWithPhoneRepository, IUserWithPhoneRepositoryData } from "@domain/user";
 
-export class UserMemoryRepository implements IUserRepository {
-    private users: IUserRepositoryData[] = [];
+export class UserMemoryRepository implements IUserRepository, IUserWithPhoneRepository {
+    private users: (IUserRepositoryData | IUserWithPhoneRepositoryData)[] = [];
 
     constructor() {
         this.users = [{
             id: 1,
-            email: "user1@example.com"
+            email: "user1@example.com",
+        }, {
+            id: 2,
+            email: "user2@example.com",
+            phone: "098-765-4321"
         }]
     }
 
     getById(id: number): Promise<IUserRepositoryData> {
-        const user = this.users.find(user => user.id === id);
-        if (!user) {
+        const userData = this.users.find(user => user.id === id);
+        if (!userData) {
             throw new Error(`User with id ${id} not found`);
         }
-        return Promise.resolve(user);
+        return Promise.resolve(userData);
+    }
+
+    getWithPhoneById(id: number): Promise<IUserWithPhoneRepositoryData> {
+        const userData = this.users.find(user => user.id === id);
+        if (!userData) {
+            throw new Error(`User with id ${id} not found`);
+        }
+        return Promise.resolve({
+            ...userData,
+            phone: (userData as IUserWithPhoneRepositoryData).phone || ""
+        });
     }
 }
